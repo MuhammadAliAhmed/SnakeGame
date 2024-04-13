f36887fb934050a24af132937d998188c01a5c2b
MuhammadAliAhmed <aliahmedali15975@gmail.com>
2024-04-13 12:23:02 +0400

    first commit

---
+++ src/Snake_Game/PanelTest.java
@@ -0,0 +1,14 @@
+package test;
+
+import Snake_Game.Panel;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PanelTest {
+    @Test
+    void keypressedtest(){
+        Panel keypressed=new Panel();
+    }
+
+}
